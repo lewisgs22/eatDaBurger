26d6239ee383cd896a14154bffaa2f46adfebcd7
Lewis Gregerson-Spencer <lewisgregerson-spencer@Lewiss-MacBook.local>
2018-11-16 09:46:21 -0800

    initial commit

---
+++ DB/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sriracha Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mac n Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Deluxe', true);
